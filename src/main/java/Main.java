6f796bf684a105a8df51403a71c3e6e0900074ef
anattoly <anattoly.k@gmail.com>
2019-07-04 07:25:06 +0300

    added implements PublicLibrary.class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader (System.in));
-        System.out.println("Please input date in format dd.mm.yyyy");
-        String date = reader.readLine();
-        LocalDate regisrDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        System.out.println(regisrDate);
+        PublicLibraries pb = new PublicLibraries();
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader (System.in));
+            System.out.println("Please input date in format dd.mm.yyyy");
+            String date = reader.readLine();
+            LocalDate regisrDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+            System.out.println(pb.getBooksOfDate(regisrDate));
+
+        } catch (DateTimeParseException o){
+            System.out.println("You entered date not format");
+        }
+
+
+
+
     }
 }
--- src/main/java/PublicLibraries.java
+++ src/main/java/PublicLibraries.java
@@ -7,6 +7,28 @@ import java.util.TreeMap;
 public class PublicLibraries {
 
 
-    Map<LocalDate, String> registeringBooks = new TreeMap<>();
-    
+    private Map<LocalDate, String> registeringBooks;
+
+    PublicLibraries() {
+        registeringBooks = new TreeMap<>();
+        registeringBooks.put(LocalDate.parse("2019-02-02"), "In Search of Lost Time by Marcel Proust");
+        registeringBooks.put(LocalDate.parse("2003-03-03"), "Don Quixote by Miguel de Cervantes");
+        registeringBooks.put(LocalDate.parse("2008-02-27"), "Ulysses by James Joyce");
+        registeringBooks.put(LocalDate.parse("2015-12-31"), "The Great Gatsby by F. Scott Fitzgerald");
+        registeringBooks.put(LocalDate.parse("2018-04-15"), "Moby Dick by Herman Melville");
+        registeringBooks.put(LocalDate.parse("2019-07-01"), "Hamlet by William Shakespeare");
+        registeringBooks.put(LocalDate.parse("2019-05-02"), "War and Peace by Leo Tolstoy");
+        registeringBooks.put(LocalDate.parse("2019-02-05"), "The Odyssey by Homer");
+    }
+
+    public String getBooksOfDate(LocalDate registerDate){
+
+        String books = registeringBooks.getOrDefault(registerDate, "No books found for entered date");
+
+        return books;
+    }
+
+
+
+
 }
